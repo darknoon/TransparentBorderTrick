438bb5f7103407ea25ee740793da1d7489b5051f
Andrew Pouliot <andrew@darknoon.com>
2012-02-27 21:01:10 -0800

    Initial Commit

---
+++ TransparentBorderTrick/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TransparentBorderTrick
+//
+//  Created by Andrew Pouliot on 2/27/12.
+//  Copyright (c) 2012 Darknoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TransparentBorderTrick/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TransparentBorderTrick
+//
+//  Created by Andrew Pouliot on 2/27/12.
+//  Copyright (c) 2012 Darknoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
